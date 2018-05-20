7528dd91e08107d17b781261eb25c238c8e15340
djopovitch <djopovitch@gmail.com>
2018-05-20 02:30:21 +0000

    Test de Conversion d'un PDF en un fichier Text avec Java exten PdfBox2.0.9

---
+++ src/main/java/test/com/App.java
@@ -0,0 +1,76 @@
+package test.com;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException
+    {
+    	
+    	PDFManager pdfManager = new PDFManager();
+	    pdfManager.setFilePath("F:\\Work\\JAVA\\LibSoftware\\testPdfBox1.pdf");
+	//System.out.println(pdfManager.ToText()); 
+	    
+	    
+    	BufferedWriter writer = null;
+    	try
+    	{
+    	    writer = new BufferedWriter( new FileWriter("F:\\Work\\JAVA\\LibSoftware\\a.txt"));
+    	    writer.write(pdfManager.ToText());
+
+    	}
+    	catch ( IOException e)
+    	{
+    	}
+    	finally
+    	{
+    	    try
+    	    {
+    	        if ( writer != null)
+    	        writer.close( );
+    	    }
+    	    catch ( IOException e)
+    	    {
+    	    }
+    	}
+    /*	PDFManager pdfManager = new PDFManager();
+	    pdfManager.setFilePath("F:\\Work\\JAVA\\LibSoftware\\testPdfBox1.pdf");
+	  // System.out.println(pdfManager.getClass().getSuperclass()); 
+	   // System.out.println(pdfManager.ToText());  
+	    File dest =new File("F:\\Work\\JAVA\\LibSoftware\\a.txt");
+
+	//	FileInputStream fis = new FileInputStream(pdfManager.ToText());
+		InputStreamReader isr = new InputStreamReader(pdfManager.toString());
+		BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+
+		FileWriter fstream = new FileWriter(dest, true);
+		BufferedWriter out = new BufferedWriter(fstream);
+
+		String aLine = null;
+		while ((aLine = in.readLine()) != null) {
+			//Process each line and add output to Dest.txt file
+			out.write(aLine);
+			out.newLine();
+		}
+
+		// do not forget to close the buffer reader
+		in.close();
+
+		// close buffer writer
+		out.close();
+		System.out.println("fin");*/
+    
+    }
+}
